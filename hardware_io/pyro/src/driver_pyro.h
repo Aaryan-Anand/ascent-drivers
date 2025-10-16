92ffb26b404525854224a3fc1cb07d54544a60fe
Aaryan-Anand <aaryan.anand15@gmail.com>
2025-10-16 15:17:53 -0700

    Removed vTaskDelays and rewrote pyro driver

--- hardware_io/pyro/src/driver_pyro.h
+++ hardware_io/pyro/src/driver_pyro.h
@@ -14,11 +14,20 @@ typedef enum {
     PYRO_CHANNEL_4 = 4
 } pyro_channel_t;
 
+typedef struct {
+    uint64_t start_us;
+    uint8_t duration_ms;
+    bool state;
+
+}pyro_state_t;
+
 // Function prototypes
 esp_err_t pyro_init(void);
 bool pyro_continuity(pyro_channel_t channel);
-double pyro_resistance(pyro_channel_t channel);
 esp_err_t pyro_activate(pyro_channel_t channel, uint8_t delay, bool bypass);
+esp_err_t pyro_deactivate(pyro_channel_t channel);
+esp_err_t pyro_update_state(void);
+esp_err_t pyro_poll_state(pyro_state_t *state[5]);
 void pyro_deinit(void);
 
 #endif /* DRIVER_PYRO_H */
